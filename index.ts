6ab5d13b7edc14f1fbe60e351d98c4e8dcbec031
danhort <danielhortonwellings@gmail.com>
2026-03-12 16:49:25 -0500

    Find food avoid obstacles

--- index.ts
+++ index.ts
@@ -10,6 +10,7 @@ function info(): InfoResponse {
     color: "#000000",
     head: "tongue",
     tail: "bolt",
+    version: "0.2.0",
   };
 }
 
@@ -21,7 +22,7 @@ function end(gameState: GameState): void {
   console.log("GAME OVER\n");
 }
 
-function getMoves(gameState: GameState): { [key: string]: boolean } {
+function getSafeMoves(gameState: GameState) {
   let moves: { [key: string]: boolean } = {
     up: true,
     down: true,
@@ -29,52 +30,81 @@ function getMoves(gameState: GameState): { [key: string]: boolean } {
     right: true,
   };
 
-  const headCoord = gameState.you.body[0];
   const boardWidth = gameState.board.width;
   const boardHeight = gameState.board.height;
+  const headCoord = gameState.you.body[0];
   const bodyCoords = gameState.you.body;
+  const otherSnakeCoords = gameState.board.snakes.flatMap((snake) => snake.body);
 
   if (
-    headCoord.x === 0 &&
-    bodyCoords.some((coord) => coord.x === headCoord.x - 1 && coord.y === headCoord.y)
+    headCoord.x === 0 ||
+    bodyCoords.some((coord) => coord.x === headCoord.x - 1 && coord.y === headCoord.y) ||
+    otherSnakeCoords.some((coord) => coord.x === headCoord.x - 1 && coord.y === headCoord.y)
   ) {
     moves.left = false;
   }
 
   if (
-    headCoord.x === boardWidth - 1 &&
-    bodyCoords.some((coord) => coord.x === headCoord.x + 1 && coord.y === headCoord.y)
+    headCoord.x === boardWidth - 1 ||
+    bodyCoords.some((coord) => coord.x === headCoord.x + 1 && coord.y === headCoord.y) ||
+    otherSnakeCoords.some((coord) => coord.x === headCoord.x + 1 && coord.y === headCoord.y)
   ) {
     moves.right = false;
   }
 
   if (
-    headCoord.y === 0 &&
-    bodyCoords.some((coord) => coord.x === headCoord.x && coord.y === headCoord.y - 1)
+    headCoord.y === 0 ||
+    bodyCoords.some((coord) => coord.x === headCoord.x && coord.y === headCoord.y - 1) ||
+    otherSnakeCoords.some((coord) => coord.x === headCoord.x && coord.y === headCoord.y - 1)
   ) {
     moves.down = false;
   }
 
   if (
-    headCoord.y === boardHeight - 1 &&
-    bodyCoords.some((coord) => coord.x === headCoord.x && coord.y === headCoord.y + 1)
+    headCoord.y === boardHeight - 1 ||
+    bodyCoords.some((coord) => coord.x === headCoord.x && coord.y === headCoord.y + 1) ||
+    otherSnakeCoords.some((coord) => coord.x === headCoord.x && coord.y === headCoord.y + 1)
   ) {
     moves.up = false;
   }
 
-  return moves;
+  const safeMoves = Object.keys(moves).filter((key) => moves[key]);
+
+  return safeMoves;
+}
+
+function getPreferredMove(gameState: GameState) {
+  const safeMoves = getSafeMoves(gameState);
+  const headCoord = gameState.you.body[0];
+
+  const closestFoodCoords = gameState.board.food.reduce(
+    (closestFood, food) => {
+      const distance = Math.abs(food.x - headCoord.x) + Math.abs(food.y - headCoord.y);
+      return distance < closestFood.distance ? { coord: food, distance: distance } : closestFood;
+    },
+    { coord: null, distance: Infinity } as {
+      coord: { x: number; y: number } | null;
+      distance: number;
+    }
+  );
+
+  if (closestFoodCoords.coord) {
+    if (closestFoodCoords.coord.x < headCoord.x && safeMoves.includes("left")) {
+      return "left";
+    } else if (closestFoodCoords.coord.x > headCoord.x && safeMoves.includes("right")) {
+      return "right";
+    } else if (closestFoodCoords.coord.y < headCoord.y && safeMoves.includes("down")) {
+      return "down";
+    } else if (closestFoodCoords.coord.y > headCoord.y && safeMoves.includes("up")) {
+      return "up";
+    }
+  }
+
+  return safeMoves[Math.floor(Math.random() * safeMoves.length)];
 }
 
 function move(gameState: GameState): MoveResponse {
-  const moves = getMoves(gameState);
-  const safeMoves = Object.keys(moves).filter((key) => moves[key]);
-
-  if (safeMoves.length == 0) {
-    console.log(`MOVE ${gameState.turn}: No safe moves detected! Moving down`);
-    return { move: "down", shout: "Bummer, no more safe moves for me!" };
-  }
-
-  const nextMove = safeMoves[Math.floor(Math.random() * safeMoves.length)];
+  const nextMove = getPreferredMove(gameState);
   console.log(`MOVE ${gameState.turn}: ${nextMove}`);
   return { move: nextMove };
 }
